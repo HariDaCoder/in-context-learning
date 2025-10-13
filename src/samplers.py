4288f99f77f09baeb857a31fa25600677ea3a9e3
HoangTimothy <hoangtimothy2510@gmail.com>
2025-10-14 00:02:45 +0700

    NST1

--- src/samplers.py
+++ src/samplers.py
@@ -15,12 +15,9 @@ def get_data_sampler(data_name, n_dims, **kwargs):
     names_to_classes = {
         "gaussian": GaussianSampler,
         "ar1":AR1Sampler,
-<<<<<<< HEAD
         # "var1":VAR1Sampler,
         "ar2":AR2Sampler,
         "vr2":VR2Sampler,
-=======
->>>>>>> 767436f (Update fetures)
     }
     if data_name in names_to_classes:
         sampler_cls = names_to_classes[data_name]
@@ -333,3 +330,45 @@ class VR2Sampler(DataSampler):
             
         return xs_b
  
+class NonStationarySampler(DataSampler):
+    def __init__(self, n_dims, coef_base=0.5, coef_amplitude=0.4, noise_std = 0.1,  bias=None, scale=None):
+        super().__init__(n_dims)
+        self.coef_base = float(coef_base)
+        self.coef_amplitude = float(coef_amplitude)
+        self.coef_noise_std = float(noise_std)
+        self.scale = scale
+        self.bias = bias
+    def get_transition_matrix(self, t, n_points):
+        t_norm = t / (n_points - 1) if n_points > 1 else 0.0
+        time_varying_factor = self.coef_base + self.coef_amplitude * torch.sin(2 * math.pi * t_norm)
+        A_t = time_varying_factor * torch.eye(self.n_dims)
+        return A_t
+
+    def sample_xs(self, n_points, b_size, n_dims_truncated=None, seeds=None):
+        xs_b = torch.zeros(b_size, n_points, self.n_dims)
+        generators = None
+        if seeds is None:
+            assert len(seeds) == b_size
+            generators = [torch.Generator().manual_seed(int(seed)) for seed in seeds]
+        if generators is None:
+            xs_b[:,0,:] = torch.randn(b_size, self.n_dims) * self.noise_std
+        else:
+            for i in range(b_size):
+                xs_b[i, 0, :] = torch.randn(self.n_dims, generator=generators[i]) * self.noise_std
+        for t in range(1, n_points):
+            A_t = self.get_transition_matrix(t, n_points)
+
+            if generators is None:
+                eps_t = self.noise_std * torch.randn(b_size, self.n_dims)
+            else:
+                eps_t = torch.zeros(b_size, self.n_dims)
+                for i in range(b_size):
+                    eps_t[i] = self.noise_std * torch.randn(self.n_dims, generator=generators[i])
+            xs_b[:, t, :] = (torch.matmul(xs_b[:, t-1, :], A_t) + eps_t)
+
+        if self.scale is not None:
+            xs_b = xs_b @ self.scale
+        if self.bias is not None:
+            xs_b += self.bias
+        
+        return xs_b
