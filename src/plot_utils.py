1644465914da70082f6c146b2f11a2f6f4906f09
HoangTimothy <hoangtimothy2510@gmail.com>
2025-11-11 13:38:35 +0700

    figure 3 and 4

--- src/plot_utils.py
+++ src/plot_utils.py
@@ -98,7 +98,7 @@ def basic_plot(metrics, models=None, trivial=1.0):
     ax.set_xlabel("in-context examples")
     ax.set_ylabel("squared error")
     ax.set_xlim(-1, len(low) + 0.1)
-    ax.set_ylim(-0.1, 1)
+    ax.set_ylim(-0.1, 2)
 
 
     legend = ax.legend(loc="upper left", bbox_to_anchor=(1, 1))
