c6b519e3f363b40709281b27ed8578c225700123
HoangTimothy <hoangtimothy2510@gmail.com>
2025-12-12 00:35:04 +0700

    fix error

--- src/eval.py
+++ src/eval.py
@@ -277,14 +277,14 @@ def build_evals(conf):
     
     # task_name =["linear_regression" if task_name == "ar1_linear_regression" else task_name][0]
     if task_name not in ["linear_regression", "ar1_linear_regression"]:
-    if task_name != "linear_regression":
-        if task_name in ["relu_2nn_regression"]:
-            evaluation_kwargs["linear_regression"] = {"task_name": "linear_regression"}
-        for name, kwargs in evaluation_kwargs.items():
-            # allow kwargs to override base_kwargs values
-            evaluation_kwargs[name] = base_kwargs.copy()
-            evaluation_kwargs[name].update(kwargs)
-        return evaluation_kwargs
+        if task_name != "linear_regression":
+            if task_name in ["relu_2nn_regression"]:
+                evaluation_kwargs["linear_regression"] = {"task_name": "linear_regression"}
+            for name, kwargs in evaluation_kwargs.items():
+                # allow kwargs to override base_kwargs values
+                evaluation_kwargs[name] = base_kwargs.copy()
+                evaluation_kwargs[name].update(kwargs)
+            return evaluation_kwargs
 
     for strategy in [
         "random_quadrants",
